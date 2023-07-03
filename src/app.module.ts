4c8789d6c28f61ef092d1ef50be6c14526e9aebe
MahinMuhammad <amimahinurrahman@gmail.com>
2023-07-03 14:02:57 +0600

    staff.dto and staff controller+service+module added in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StaffController } from './staff/staff.controller';
+import { StaffService } from './staff/staff.service';
+import { StaffModule } from './staff/staff.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [StaffModule],
+  controllers: [AppController, StaffController],
+  providers: [AppService, StaffService],
 })
 export class AppModule {}
