e46eeb89c2edced5200f8da03f9b2256441d06cf
MahinMuhammad <amimahinurrahman@gmail.com>
2023-07-03 17:00:22 +0600

    sales_report.dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,13 @@ import { StaffModule } from './staff/staff.module';
 import { CategoryController } from './category/category.controller';
 import { CategoryService } from './category/category.service';
 import { CategoryModule } from './category/category.module';
+import { SalesReportController } from './sales_report/sales_report.controller';
+import { SalesReportService } from './sales_report/sales_report.service';
+import { SalesReportModule } from './sales_report/sales_report.module';
 
 @Module({
-  imports: [StaffModule, CategoryModule],
-  controllers: [AppController, StaffController, CategoryController],
-  providers: [AppService, StaffService, CategoryService],
+  imports: [StaffModule, CategoryModule, SalesReportModule],
+  controllers: [AppController, StaffController, CategoryController, SalesReportController],
+  providers: [AppService, StaffService, CategoryService, SalesReportService],
 })
 export class AppModule {}
---
+++ src/sales_report/sales_report.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('sales-report')
+export class SalesReportController {}
