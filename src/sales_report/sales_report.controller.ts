8a2028731cb626bb4cb0d283bb48a3216aa632b2
MahinMuhammad <amimahinurrahman@gmail.com>
2023-07-03 17:34:27 +0600

    sales_report module v1

--- src/sales_report/sales_report.controller.ts
+++ src/sales_report/sales_report.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { SalesReportDto } from './dto/sales_report.dto';
+import { SalesReportService } from './sales_report.service';
 
-@Controller('sales-report')
-export class SalesReportController {}
+@Controller('sales_report')
+export class SalesReportController {
+    constructor(private salesReportService: SalesReportService){}
+
+    @Get()
+    getAllReports(){
+        return this.salesReportService.getAllReports();
+    }
+
+    @Get(':sales_id')
+    getSingleReport(@Param('sales_id', ParseIntPipe) id: number){
+        return this.salesReportService.getSingleReport(id);
+    }
+
+    @Post()
+    addReport(@Body() report: SalesReportDto){
+        return this.salesReportService.addReport(report);
+    }
+
+    @Patch(':sales_id')
+    updateReport(@Body() report: SalesReportDto, @Param('sales_id', ParseIntPipe) id: number){
+        return this.salesReportService.updateReport(report, id);
+    }
+
+    @Delete(':sales_id')
+    removeReport(@Param('sales_id', ParseIntPipe) id: number){
+        return this.salesReportService.removeReport(id);
+    }
+}
