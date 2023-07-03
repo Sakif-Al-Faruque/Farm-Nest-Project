0b55b4fe76dc035db1faf0435c451ccd036a55af
MahinMuhammad <amimahinurrahman@gmail.com>
2023-07-03 14:38:06 +0600

    category.service

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { CategoryDto } from './dto/category.dto';
+import { CategoryService } from './category.service';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+    constructor(private categoryService: CategoryService){}
+
+    @Get()
+    ShowAllCategories(){
+        
+    }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { CategoryDto } from './dto/category.dto';
+
+let categories = [
+    {
+        ca_id: 324521,
+        caname: "Agro Food",
+        unit: 8
+    },
+    {
+        ca_id: 923734,
+        caname: "Medicine",
+        unit: 5
+    }
+]
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    getAllCategories(){
+        return categories;
+    }
+
+    getSingleCategory(caid: number){
+        return categories.find((category)=> category.ca_id === caid);
+    }
+
+    addCategory(category: CategoryDto){
+        categories.push(category);
+        return 'Category Added';
+    }
+}
