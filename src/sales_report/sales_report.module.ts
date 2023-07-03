626c40b193774b60cf4a359cb64d3d08d6d29fec
MahinMuhammad <amimahinurrahman@gmail.com>
2023-07-03 17:02:18 +0600

    sales_report.module

--- src/sales_report/sales_report.module.ts
+++ src/sales_report/sales_report.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { SalesReportController } from './sales_report.controller';
+import { SalesReportService } from './sales_report.service';
 
-@Module({})
+@Module({
+    controllers: [SalesReportController],
+    providers: [SalesReportService],
+})
 export class SalesReportModule {}
